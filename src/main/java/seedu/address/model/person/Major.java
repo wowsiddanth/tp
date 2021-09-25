e5d7d592d6fb2c68e020563e3e83be05e01f311d
wowsiddanth <xpssiddanthratan@gmail.com>
2021-09-25 15:42:46 +0800

    Add full implementation of Major class

--- src/main/java/seedu/address/model/person/Major.java
+++ src/main/java/seedu/address/model/person/Major.java
@@ -1,10 +1,7 @@
 package seedu.address.model.person;
 
-import com.fasterxml.jackson.databind.ext.CoreXMLDeserializers;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Optional;
 
 import static java.util.Objects.requireNonNull;
@@ -26,6 +23,10 @@ public class Major {
      */
     public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
+    /*
+     * The HashMap that maps the major, to the list of students that are currently
+     * in that major.
+     */
     public static HashMap<Major, ArrayList<Person>> studentsToMajors = new HashMap<>();
 
     public final String value;
@@ -41,17 +42,34 @@ public class Major {
         value = major;
     }
 
-    public static void addStudent(Person person) {
-        Major majorOfStudent = person.getMajor();
+    /**
+     * Adds a student to the array list corresponding to that student's major
+     *
+     * @param student The student to be added
+     */
+    public static void addStudent(Person student) {
+        Major majorOfStudent = student.getMajor();
         Optional<ArrayList<Person>> currStudents = Optional.ofNullable(studentsToMajors.get(majorOfStudent));
+
         if (currStudents.isEmpty()) {
             ArrayList<Person> studentsInMajor = new ArrayList<>();
-            studentsInMajor.add(person);
+            studentsInMajor.add(student);
             studentsToMajors.put(majorOfStudent, studentsInMajor);
         } else {
-            currStudents.get().add(person);
+            currStudents.get().add(student);
             studentsToMajors.put(majorOfStudent, currStudents.get());
         }
+    }
+
+    /**
+     * Remove student from the arraylist corresponding to the student's major
+     *
+     * @param student The student to be removed
+     */
+    public static void removeStudent(Person student) {
+        Major majorOfStudent = student.getMajor();
+        ArrayList<Person> currStudents = studentsToMajors.get(majorOfStudent);
+        currStudents.remove(student);
     }
 
     /**
@@ -63,8 +81,13 @@ public class Major {
         return matchesRegex && validCourse;
     }
 
-    public static boolean isValidMajorName(String course) {
-        return course.equals("CS") || course.equals("IS") ||course.equals("ISEC") || course.equals("BZA");
+    /**
+     * Checks if the given major string is a valid major name
+     * @param major The major given
+     * @return True if valid, false if invalid
+     */
+    public static boolean isValidMajorName(String major) {
+        return major.equals("CS") || major.equals("IS") || major.equals("ISEC") || major.equals("BZA");
     }
 
     @Override
