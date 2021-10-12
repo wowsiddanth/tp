3f27938c7eeea6c4059d134a1b03da42a8292a5b
rehad-a <rahaad@hotmail.com>
2021-10-13 01:23:19 +0800

    Fix checkstyle errors

--- src/test/java/nustracker/logic/commands/FilterEventCommandTest.java
+++ src/test/java/nustracker/logic/commands/FilterEventCommandTest.java
@@ -1,21 +1,20 @@
 package nustracker.logic.commands;
 
-import static nustracker.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static nustracker.logic.commands.CommandTestUtil.assertCommandSuccess;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
-import java.util.Collections;
-
+//import java.util.Arrays;
+//import java.util.Collections;
 import org.junit.jupiter.api.Test;
 
-import nustracker.commons.core.Messages;
+//import nustracker.commons.core.Messages;
 import nustracker.model.Model;
 import nustracker.model.ModelManager;
 import nustracker.model.UserPrefs;
 import nustracker.model.student.EnrolledEventsContainsKeywordsPredicate;
-import nustracker.testutil.TypicalEvents;
+//import nustracker.testutil.TypicalEvents;
 import nustracker.testutil.TypicalStudents;
 
 
@@ -57,7 +56,8 @@ public class FilterEventCommandTest {
     //    @Test
     //    public void execute_oneEvent_multipleStudentsFound() {
     //        String expectedMessage = String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW, 2);
-    //        EnrolledEventsContainsKeywordsPredicate predicate = preparePredicate(TypicalEvents.ORIENTATION.getName().eventName);
+    //        EnrolledEventsContainsKeywordsPredicate predicate = preparePredicate(
+    //          TypicalEvents.ORIENTATION.getName().eventName);
     //        FilterCommand command = new FilterEventCommand(predicate);
     //        expectedModel.updateFilteredStudentList(predicate);
     //        CommandTestUtil.assertCommandSuccess(command, model, expectedMessage, expectedModel);
