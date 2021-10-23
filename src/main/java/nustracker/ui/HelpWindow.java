2d089212030d8bcaef2431d012c4a5056393a284
YuPei <sunyupei1999@gmail.com>
2021-10-23 23:13:36 +0800

    set up all the buttons and linked everything properly in HelpWindow.java

--- src/main/java/nustracker/ui/HelpWindow.java
+++ src/main/java/nustracker/ui/HelpWindow.java
@@ -24,75 +24,144 @@ public class HelpWindow extends UiPart<Stage> {
     public static final String HELP_MESSAGE = "Refer to the user guide: ";
     public static final String COPY_LINK_TEXT = "Open User Guide";
 
+    // STUDENT COMMANDS
     public static final String ADD_COMMAND = "add";
-    public static final String ADD_COMMAND_EXAMPLE = ADD_COMMAND
-            + " n/STUDENT_NAME m/MAJOR id/STUDENT_ID y/YEAR p/NUMBER e/EMAIL [ev/EVENT]";
-
-    public static final String FILTER_COMMAND = "filter";
-    public static final String FILTER_COMMAND_EXAMPLE = FILTER_COMMAND
-            + " [id/STUDENT_ID] [ev/EVENT_NAME] [n/STUDENT_NAME]";
-
-    public static final String EDIT_COMMAND = "edit";
-    public static final String EDIT_COMMAND_EXAMPLE = EDIT_COMMAND
-            + " INDEX [n/NAME] [m/MAJOR] [id/Student ID] [y/YEAR] [p/PHONE] [e/EMAIL] [ev/EVENT]";
-
-    public static final String CREATE_COMMAND = "create";
-    public static final String CREATE_COMMAND_EXAMPLE = CREATE_COMMAND;
-
-    public static final String ENROLL_COMMAND = "enroll";
-    public static final String ENROLL_COMMAND_EXAMPLE = ENROLL_COMMAND;
-
-    public static final String DELETE_COMMAND = "delete";
-    public static final String DELETE_COMMAND_EXAMPLE = DELETE_COMMAND + " INDEX";
-
-    public static final String LIST_COMMAND = "list";
-    public static final String LIST_COMMAND_EXAMPLE = LIST_COMMAND;
-
-    public static final String EXIT_COMMAND = "exit";
-    public static final String EXIT_COMMAND_EXAMPLE = EXIT_COMMAND;
+    public static final String ADD_COMMAND_EXAMPLE = " " + ADD_COMMAND
+            + " n/STUDENT_NAME m/MAJOR id/STUDENT_ID y/YEAR p/NUMBER e/EMAIL";
 
     public static final String STUDENTS_COMMAND = "students";
+    public static final String STUDENTS_COMMAND_EXAMPLE = " " + STUDENTS_COMMAND;
+
+    public static final String EDIT_COMMAND = "edit";
+    public static final String EDIT_COMMAND_EXAMPLE = " " + EDIT_COMMAND
+            + " [id/ID_OF_STUDENT_TO_EDIT] [n/NAME] [m/MAJOR] [id/NEW_STUDENT_ID] [y/YEAR] [p/PHONE] [e/EMAIL]";
+
+    public static final String DELETE_COMMAND = "delete";
+    public static final String DELETE_COMMAND_EXAMPLE1 = " " + DELETE_COMMAND + " id/STUDENT_ID";
+
+    public static final String FILTER_COMMAND = "filter";
+    public static final String FILTER_COMMAND_EXAMPLE1 = " " + FILTER_COMMAND
+            + " n/STUDENT_NAME [MORE_STUDENT_NAMES]...";
+    public static final String FILTER_COMMAND_EXAMPLE2 = " " + FILTER_COMMAND
+            + " id/STUDENT_ID [MORE_STUDENT_IDS]...";
+    public static final String FILTER_COMMAND_EXAMPLE3 = " " + FILTER_COMMAND
+            + " m/MAJOR [MORE_MAJORS]...";
+    public static final String FILTER_COMMAND_EXAMPLE4 = " " + FILTER_COMMAND
+            + " y/YEAR [MORE_YEARS]...";
+    public static final String FILTER_COMMAND_EXAMPLE5 = " " + FILTER_COMMAND
+            + " ev/EVENT_NAME";
+
+
+    // EVENT COMMANDS
+    public static final String CREATE_COMMAND = "create";
+    public static final String CREATE_COMMAND_EXAMPLE = " " + CREATE_COMMAND
+            + " n/EVENT_NAME d/EVENT_DATE t/EVENT_TIME";
 
     public static final String EVENTS_COMMAND = "events";
+    public static final String EVENTS_COMMAND_EXAMPLE = " " + EVENTS_COMMAND;
+
+    public static final String ENROLL_COMMAND = "enroll";
+    public static final String ENROLL_COMMAND_EXAMPLE = " " + ENROLL_COMMAND
+            + " id/STUDENT_ID ev/EVENT_NAME";
+
+    public static final String REMOVE_COMMAND = "remove";
+    public static final String REMOVE_COMMAND_EXAMPLE = " " + REMOVE_COMMAND
+            + " id/STUDENT_ID ev/EVENT_NAME";
+
+    public static final String DELETE_COMMAND_EXAMPLE2 = " " + DELETE_COMMAND
+            + " ev/EVENT_NAME";
+
+
+    // OTHER COMMANDS
+    public static final String HELP_COMMAND = "help";
+    public static final String HELP_COMMAND_EXAMPLE = " " + HELP_COMMAND;
+
+    public static final String SETTINGS_COMMAND = "settings";
+    public static final String SETTINGS_COMMAND_EXAMPLE = " " + SETTINGS_COMMAND;
+
+    public static final String EXPORT_COMMAND = "export";
+    public static final String EXPORT_COMMAND_EXAMPLE = " " + EXPORT_COMMAND
+            + " fn/FILE_NAME;";
+
+    public static final String EXIT_COMMAND = "exit";
+    public static final String EXIT_COMMAND_EXAMPLE = " " + EXIT_COMMAND;
+
+
+
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "NewHelpWindow.fxml";
 
     @FXML
     private VBox helpWindow;
-//
+
     @FXML
     private Text helpMessage;
-//
-//    @FXML
-//    private Text addCommandExample;
-//
-//    @FXML
-//    private Text listCommandExample;
-//
-//    @FXML
-//    private Text filterCommandExample;
-//
-//    @FXML
-//    private Text editCommandExample;
-//
-//    @FXML
-//    private Text deleteCommandExample;
-//
-//    @FXML
-//    private Text exitCommandExample;
-//
-//    @FXML
-//    private Text createCommandExample;
-//
-//    @FXML
-//    private Text enrollCommandExample;
-//
-//    @FXML
-//    private Text studentsCommandExample;
-//
-//    @FXML
-//    private Text eventsCommandExample;
+
+
+    // STUDENT COMMANDS
+    @FXML
+    private Text addCommandExample;
+
+    @FXML
+    private Text studentsCommandExample;
+
+    @FXML
+    private Text editCommandExample;
+
+    @FXML
+    private Text deleteCommandExample1;
+
+    @FXML
+    private Text filterCommandExample1;
+
+    @FXML
+    private Text filterCommandExample2;
+
+    @FXML
+    private Text filterCommandExample3;
+
+    @FXML
+    private Text filterCommandExample4;
+
+    @FXML
+    private Text filterCommandExample5;
+
+
+
+    // EVENT COMMANDS
+    @FXML
+    private Text createCommandExample;
+
+    @FXML
+    private Text eventsCommandExample;
+
+    @FXML
+    private Text enrollCommandExample;
+
+    @FXML
+    private Text removeCommandExample;
+
+    @FXML
+    private Text deleteCommandExample2;
+
+
+
+    // OTHER COMMANDS
+    @FXML
+    private Text helpCommandExample;
+
+    @FXML
+    private Text settingsCommandExample;
+
+    @FXML
+    private Text exportCommandExample;
+
+    @FXML
+    private Text exitCommandExample;
+
+
+
 
     /**
      * Creates a new HelpWindow.
@@ -103,16 +172,30 @@ public class HelpWindow extends UiPart<Stage> {
         super(FXML, root);
 
         helpMessage.setText(HELP_MESSAGE);
-//        addCommandExample.setText(ADD_COMMAND_EXAMPLE);
-//        filterCommandExample.setText(FILTER_COMMAND_EXAMPLE);
-//        editCommandExample.setText(EDIT_COMMAND_EXAMPLE);
-//        deleteCommandExample.setText(DELETE_COMMAND_EXAMPLE);
-//        listCommandExample.setText(LIST_COMMAND_EXAMPLE);
-//        exitCommandExample.setText(EXIT_COMMAND_EXAMPLE);
-//        createCommandExample.setText(CREATE_COMMAND_EXAMPLE);
-//        enrollCommandExample.setText(ENROLL_COMMAND_EXAMPLE);
-//        studentsCommandExample.setText(STUDENTS_COMMAND);
-//        eventsCommandExample.setText(EVENTS_COMMAND);
+
+        // STUDENTS COMMANDS
+        addCommandExample.setText(ADD_COMMAND_EXAMPLE);
+        studentsCommandExample.setText(STUDENTS_COMMAND_EXAMPLE);
+        editCommandExample.setText(EDIT_COMMAND_EXAMPLE);
+        deleteCommandExample1.setText(DELETE_COMMAND_EXAMPLE1);
+        filterCommandExample1.setText(FILTER_COMMAND_EXAMPLE1);
+        filterCommandExample2.setText(FILTER_COMMAND_EXAMPLE2);
+        filterCommandExample3.setText(FILTER_COMMAND_EXAMPLE3);
+        filterCommandExample4.setText(FILTER_COMMAND_EXAMPLE4);
+        filterCommandExample5.setText(FILTER_COMMAND_EXAMPLE5);
+
+        // EVENT COMMANDS
+        createCommandExample.setText(CREATE_COMMAND_EXAMPLE);
+        eventsCommandExample.setText(EVENTS_COMMAND_EXAMPLE);
+        enrollCommandExample.setText(ENROLL_COMMAND_EXAMPLE);
+        removeCommandExample.setText(REMOVE_COMMAND_EXAMPLE);
+        deleteCommandExample2.setText(DELETE_COMMAND_EXAMPLE2);
+
+        // OTHER COMMANDS
+        helpCommandExample.setText(HELP_COMMAND_EXAMPLE);
+        settingsCommandExample.setText(SETTINGS_COMMAND_EXAMPLE);
+        exportCommandExample.setText(EXPORT_COMMAND_EXAMPLE);
+        exitCommandExample.setText(EXIT_COMMAND_EXAMPLE);
     }
 
     /**
@@ -182,6 +265,7 @@ public class HelpWindow extends UiPart<Stage> {
         }
     }
 
+    // STUDENT COMMANDS ================================================================================================
     /**
      * Copies the AddCommand to the user guide to the clipboard.
      */
@@ -194,24 +278,13 @@ public class HelpWindow extends UiPart<Stage> {
     }
 
     /**
-     * Copies the ListCommand to the user guide to the clipboard.
+     * Copies the StudentsCommand to the user guide to the clipboard.
      */
     @FXML
-    private void copyListCommand() {
+    private void copyStudentsCommand() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
-        url.putString(LIST_COMMAND_EXAMPLE);
-        clipboard.setContent(url);
-    }
-
-    /**
-     * Copies the FilterCommand to the user guide to the clipboard.
-     */
-    @FXML
-    private void copyFilterCommand() {
-        final Clipboard clipboard = Clipboard.getSystemClipboard();
-        final ClipboardContent url = new ClipboardContent();
-        url.putString(FILTER_COMMAND_EXAMPLE);
+        url.putString(STUDENTS_COMMAND_EXAMPLE);
         clipboard.setContent(url);
     }
 
@@ -227,27 +300,72 @@ public class HelpWindow extends UiPart<Stage> {
     }
 
     /**
-     * Copies the DeleteCommand to the user guide to the clipboard.
+     * Copies the DeleteCommand1 to the user guide to the clipboard.
      */
     @FXML
-    private void copyDeleteCommand() {
+    private void copyDeleteCommand1() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
-        url.putString(DELETE_COMMAND_EXAMPLE);
+        url.putString(DELETE_COMMAND_EXAMPLE1);
         clipboard.setContent(url);
     }
 
     /**
-     * Copies the ExitCommand to the user guide to the clipboard.
+     * Copies the FilterCommand1 to the user guide to the clipboard.
      */
     @FXML
-    private void copyExitCommand() {
+    private void copyFilterCommand1() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
-        url.putString(EXIT_COMMAND_EXAMPLE);
+        url.putString(FILTER_COMMAND_EXAMPLE1);
         clipboard.setContent(url);
     }
 
+    /**
+     * Copies the FilterCommand2 to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyFilterCommand2() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(FILTER_COMMAND_EXAMPLE2);
+        clipboard.setContent(url);
+    }
+
+    /**
+     * Copies the FilterCommand3 to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyFilterCommand3() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(FILTER_COMMAND_EXAMPLE3);
+        clipboard.setContent(url);
+    }
+
+    /**
+     * Copies the FilterCommand4 to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyFilterCommand4() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(FILTER_COMMAND_EXAMPLE4);
+        clipboard.setContent(url);
+    }
+
+    /**
+     * Copies the FilterCommand5 to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyFilterCommand5() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(FILTER_COMMAND_EXAMPLE5);
+        clipboard.setContent(url);
+    }
+
+    // EVENT COMMANDS ================================================================================================
     /**
      * Copies the CreateCommand to the user guide to the clipboard.
      */
@@ -256,6 +374,17 @@ public class HelpWindow extends UiPart<Stage> {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
         url.putString(CREATE_COMMAND_EXAMPLE);
+        clipboard.setContent(url);
+    }
+
+    /**
+     * Copies the EventsCommand to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyEventsCommand() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(EVENTS_COMMAND);
         clipboard.setContent(url);
     }
 
@@ -271,25 +400,73 @@ public class HelpWindow extends UiPart<Stage> {
     }
 
     /**
-     * Copies the StudentsCommand to the user guide to the clipboard.
+     * Copies the RemoveCommand to the user guide to the clipboard.
      */
     @FXML
-    private void copyStudentsCommand() {
+    private void copyRemoveCommand() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
-        url.putString(STUDENTS_COMMAND);
+        url.putString(REMOVE_COMMAND_EXAMPLE);
         clipboard.setContent(url);
     }
 
     /**
-     * Copies the EventsCommand to the user guide to the clipboard.
+     * Copies the DeleteCommand2 to the user guide to the clipboard.
      */
     @FXML
-    private void copyEventsCommand() {
+    private void copyDeleteCommand2() {
         final Clipboard clipboard = Clipboard.getSystemClipboard();
         final ClipboardContent url = new ClipboardContent();
-        url.putString(EVENTS_COMMAND);
+        url.putString(DELETE_COMMAND_EXAMPLE2);
         clipboard.setContent(url);
     }
+
+    // OTHER COMMANDS ================================================================================================
+
+    /**
+     * Copies the HelpCommand to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyHelpCommand() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(HELP_COMMAND_EXAMPLE);
+        clipboard.setContent(url);
+    }
+
+    /**
+     * Copies the SettingsCommand to the user guide to the clipboard.
+     */
+    @FXML
+    private void copySettingsCommand() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(SETTINGS_COMMAND_EXAMPLE);
+        clipboard.setContent(url);
+    }
+
+    /**
+     * Copies the ExportCommand to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyExportCommand() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(EXPORT_COMMAND_EXAMPLE);
+        clipboard.setContent(url);
+    }
+
+
+    /**
+     * Copies the ExitCommand to the user guide to the clipboard.
+     */
+    @FXML
+    private void copyExitCommand() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent url = new ClipboardContent();
+        url.putString(EXIT_COMMAND_EXAMPLE);
+        clipboard.setContent(url);
+    }
+
 
 }
