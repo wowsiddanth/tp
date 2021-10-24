8790d4b6634e97d7ccf8649084174b88fd1a5a94
wowsiddanth <xpssiddanthratan@gmail.com>
2021-10-24 16:42:31 +0800

    Fix issue of changing button sizes

--- src/main/java/nustracker/ui/HelpWindow.java
+++ src/main/java/nustracker/ui/HelpWindow.java
@@ -58,7 +58,6 @@ public class HelpWindow extends UiPart<Stage> {
     public static final String WHITELIST_COMMAND_EXAMPLE = " " + WHITELIST_COMMAND
             + " id/STUDENT_ID ev/EVENT";
 
-
     // EVENT COMMANDS
     public static final String CREATE_COMMAND = "create";
     public static final String CREATE_COMMAND_EXAMPLE = " " + CREATE_COMMAND
@@ -78,7 +77,6 @@ public class HelpWindow extends UiPart<Stage> {
     public static final String DELETE_COMMAND_EXAMPLE2 = " " + DELETE_COMMAND
             + " ev/EVENT_NAME";
 
-
     // OTHER COMMANDS
     public static final String HELP_COMMAND = "help";
     public static final String HELP_COMMAND_EXAMPLE = " " + HELP_COMMAND;
@@ -94,86 +92,59 @@ public class HelpWindow extends UiPart<Stage> {
     public static final String EXIT_COMMAND_EXAMPLE = " " + EXIT_COMMAND;
 
 
-
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
     @FXML
     private VBox helpWindow;
-
     @FXML
     private Text helpMessage;
-
 
     // STUDENT COMMANDS
     @FXML
     private Text addCommandExample;
-
     @FXML
     private Text studentsCommandExample;
-
     @FXML
     private Text editCommandExample;
-
     @FXML
     private Text deleteCommandExample1;
-
     @FXML
     private Text filterCommandExample1;
-
     @FXML
     private Text filterCommandExample2;
-
     @FXML
     private Text filterCommandExample3;
-
     @FXML
     private Text filterCommandExample4;
-
     @FXML
     private Text filterCommandExample5;
-
     @FXML
     private Text blacklistCommandExample;
-
     @FXML
     private Text whitelistCommandExample;
-
-
 
     // EVENT COMMANDS
     @FXML
     private Text createCommandExample;
-
     @FXML
     private Text eventsCommandExample;
-
     @FXML
     private Text enrollCommandExample;
-
     @FXML
     private Text removeCommandExample;
-
     @FXML
     private Text deleteCommandExample2;
-
-
 
     // OTHER COMMANDS
     @FXML
     private Text helpCommandExample;
-
     @FXML
     private Text settingsCommandExample;
-
     @FXML
     private Text exportCommandExample;
-
     @FXML
     private Text exitCommandExample;
-
-
-
 
     /**
      * Creates a new HelpWindow.
@@ -503,7 +474,5 @@ public class HelpWindow extends UiPart<Stage> {
         url.putString(EXIT_COMMAND_EXAMPLE);
         clipboard.setContent(url);
     }
-
-
 
 }
