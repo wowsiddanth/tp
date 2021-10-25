4a004a4991bd1abb6856e8df853e05b5fe43a432
TLChicken <tehlightningchicken@gmail.com>
2021-10-26 02:12:43 +0800

    Create DeleteFilteredStudentsCmd Test Cases

--- src/test/java/nustracker/logic/commands/DeleteFilteredStudentsCommandTest.java
+++ src/test/java/nustracker/logic/commands/DeleteFilteredStudentsCommandTest.java
@@ -2,7 +2,10 @@ package nustracker.logic.commands;
 
 import static nustracker.commons.core.Messages.MESSAGE_STUDENT_LIST_NOT_SHOWN;
 import static nustracker.logic.commands.CommandTestUtil.assertCommandFailureShownEventList;
+import static nustracker.logic.commands.CommandTestUtil.assertCommandFailureShownStudentList;
 import static nustracker.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static nustracker.logic.commands.DeleteFilteredStudentsCommand.MESSAGE_AT_LEAST_ONE_STUDENT_REQ_FOR_DELFILTER;
+import static nustracker.testutil.TypicalEvents.MATH_OLYMPIAD;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -14,6 +17,7 @@ import javafx.collections.ObservableList;
 import nustracker.model.Model;
 import nustracker.model.ModelManager;
 import nustracker.model.UserPrefs;
+import nustracker.model.student.EnrolledEventsContainsKeywordsPredicate;
 import nustracker.model.student.Student;
 import nustracker.testutil.TypicalStudents;
 
@@ -52,11 +56,65 @@ public class DeleteFilteredStudentsCommandTest {
     }
 
     @Test
+    public void execute_filteredList_success() {
+        // Filter by MATH_OLYMPIAD since TypicalStudents has a few student enrolled in it
+        EnrolledEventsContainsKeywordsPredicate mathOlympPred =
+                new EnrolledEventsContainsKeywordsPredicate(MATH_OLYMPIAD.getName());
+
+        model.updateFilteredStudentList(mathOlympPred);
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+        expectedModel.updateFilteredStudentList(currStudent -> true);
+
+        ObservableList<Student> refList = expectedModel.getFilteredStudentList();
+        ArrayList<Student> newList = new ArrayList<>();
+
+        for (Student currStudent : refList) {
+            if (currStudent.getEvents().isEnrolledInEvent(MATH_OLYMPIAD.getName())) {
+                newList.add(currStudent);
+            }
+        }
+
+
+        for (Student currStudent : newList) {
+            expectedModel.deleteStudent(currStudent);
+        }
+
+        DeleteFilteredStudentsCommand deleteFilteredStudentsCommand = new DeleteFilteredStudentsCommand();
+
+        assertCommandSuccess(deleteFilteredStudentsCommand, model,
+                DeleteFilteredStudentsCommand.MESSAGE_DELETE_ALL_FILTERED_SUCCESS, expectedModel);
+
+    }
+
+    @Test
+    public void execute_unfilteredListNoStudents_throwsCommandException() {
+        ModelManager currModel = new ModelManager();
+
+
+        DeleteFilteredStudentsCommand deleteFilteredStudentsCommand = new DeleteFilteredStudentsCommand();
+
+        assertCommandFailureShownStudentList(deleteFilteredStudentsCommand, currModel,
+                MESSAGE_AT_LEAST_ONE_STUDENT_REQ_FOR_DELFILTER);
+
+    }
+
+    @Test
+    public void execute_originalListHasStudentsFilteredListNoStudents_throwsCommandException() {
+        model.updateFilteredStudentList(currStudent -> false);
+
+        DeleteFilteredStudentsCommand deleteFilteredStudentsCommand = new DeleteFilteredStudentsCommand();
+
+        assertCommandFailureShownStudentList(deleteFilteredStudentsCommand, model,
+                MESSAGE_AT_LEAST_ONE_STUDENT_REQ_FOR_DELFILTER);
+    }
+
+    @Test
     public void execute_unfilteredListWhenEventsShown_throwsCommandException() {
         DeleteFilteredStudentsCommand cmd = new DeleteFilteredStudentsCommand();
 
         assertCommandFailureShownEventList(cmd, model, MESSAGE_STUDENT_LIST_NOT_SHOWN);
     }
+
 
 
 
