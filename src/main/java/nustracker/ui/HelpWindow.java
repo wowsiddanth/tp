e52554295417b90b5aba730027a7f7bf6216d3fc
wowsiddanth <xpssiddanthratan@gmail.com>
2021-11-03 21:27:10 +0800

    Fix gradle checkstyle errors

--- src/main/java/nustracker/ui/HelpWindow.java
+++ src/main/java/nustracker/ui/HelpWindow.java
@@ -13,7 +13,6 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import nustracker.commons.core.LogsCenter;
